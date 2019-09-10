18be5b13ff729c52e525c9e1fa765b9b700fd41d
João Pedro Aquino <joao.aquino@ctvoicer.com.br>
2019-09-10 09:31:57 -0300

    style (dashboardMenu): apply scss with BEM methodology

--- src/renderer/components/Paginas/dashboardMenu.vue
+++ src/renderer/components/Paginas/dashboardMenu.vue
@@ -1,24 +1,24 @@
 <template>
   <v-row class="dash">
     <v-col cols="3" class="bloco">
-      <p class="title font-weight-light">Agente: <b>{{ nomeAgente }}</b></p>
-      <p class="title font-weight-light">Grupo: <b>{{ nomeGrupo }}</b></p>
-      <p class="title font-weight-light">Disponível à: <b>{{ disponibilidade }}</b></p>
+      <p class="bloco__title font-weight-light">Agente: <b>{{ nomeAgente }}</b></p>
+      <p class="bloco__title font-weight-light">Grupo: <b>{{ nomeGrupo }}</b></p>
+      <p class="bloco__title font-weight-light">Disponível à: <b>{{ disponibilidade }}</b></p>
     </v-col>
     <v-col cols="3" class="bloco">
-      <p class="title font-weight-light">{{ ligacoesInternas }} : Ligações Internas</p>
-      <p class="title font-weight-light">{{ ligacoesEntrantes }} : Ligações Entrantes</p>
-      <p class="title font-weight-light">{{ ligacoesSaintes }} : Ligações Saintes</p>
+      <p class="bloco__title font-weight-light">{{ ligacoesInternas }} : Ligações Internas</p>
+      <p class="bloco__title font-weight-light">{{ ligacoesEntrantes }} : Ligações Entrantes</p>
+      <p class="bloco__title font-weight-light">{{ ligacoesSaintes }} : Ligações Saintes</p>
     </v-col>
     <v-col cols="3" class="bloco">
-      <p class="title font-weight-light">{{ ligacoesAtendidasSaintes }} : Ligações Atendidas/Saintes</p>
-      <p class="title font-weight-light">{{ ligacoesAbandonadas }} : Ligações Abandonadas</p>
-      <p class="title font-weight-light">{{ ligacoesTransportadas }} : Ligações Transportadas</p>
+      <p class="bloco__title font-weight-light">{{ ligacoesAtendidasSaintes }} : Ligações Atendidas/Saintes</p>
+      <p class="bloco__title font-weight-light">{{ ligacoesAbandonadas }} : Ligações Abandonadas</p>
+      <p class="bloco__title font-weight-light">{{ ligacoesTransportadas }} : Ligações Transportadas</p>
     </v-col>
-    <v-col cols="3" class="bloco-ultimo">
-      <p class="title font-weight-light">Agentes disponíveis: <v-chip color="danger" x-small pill >{{ agentesDisponiveis }}</v-chip></p>
-      <p class="title font-weight-light"></p>
-      <p class="title font-weight-light">Tempo Médio: {{ tempoMedio }}</p>
+    <v-col cols="3" class="bloco">
+      <p class="bloco__title font-weight-light">Agentes disponíveis: <v-chip color="danger" x-small pill >{{ agentesDisponiveis }}</v-chip></p>
+      <p class="bloco__title font-weight-light"></p>
+      <p class="bloco__title font-weight-light">Tempo Médio: {{ tempoMedio }}</p>
     </v-col>
   </v-row>
 </template>
@@ -47,29 +47,26 @@
   }
 </script>
 
-<style scoped>
-  .title {
-    color: rgb(243, 243, 243);
-    font-size: 11px;
-    font-weight: lighter;
-    letter-spacing: 0px;
-    margin: 0px 5px;
-    line-height: 20px;
-  }
-
+<style lang="scss" scoped>
   .dash { 
     margin-left: 5px;
     margin-top: 0px;
-    padding:0px; 
+    padding:0px;
+    .bloco {
+      border-right: solid rgba(29, 61, 114, .8) 10px;
+      padding: 0px;
+      max-height: 60px;
+      &:last-of-type {
+        border: none;
+      }
+      &__title {
+        color: rgb(243, 243, 243);
+        font-size: 11px;
+        font-weight: lighter;
+        letter-spacing: 0px;
+        margin: 0px 5px;
+        line-height: 20px;
+      }
     }
-
-  .bloco {
-    border-right: solid rgba(29, 61, 114, .8) 10px;
-    padding: 0px;
-    max-height: 60px;
-  }
-  .bloco-ultimo {
-    padding: 0px;
-    max-height: 60px;
   }
 </style>
