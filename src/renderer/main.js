310b01e2cb7e3e17fa1ab4f79572343d85dd0a16
João Pedro Aquino <joao.aquino@ctvoicer.com.br>
2019-09-12 15:39:09 -0300

    Apply changes on main.js

--- src/renderer/main.js
+++ src/renderer/main.js
@@ -1,62 +1,22 @@
 import Vue from 'vue'
-import axios from 'axios'
+import Vuetify from 'vuetify'
 
 import App from './App'
+import i18n from './config/I18n'
 import router from './router'
 import store from './store'
-import Vuetify from 'vuetify'
+
 import 'vuetify/dist/vuetify.min.css'
-//Fonts Awsome
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { faFontAwesome } from '@fortawesome/free-brands-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import {
-  faCommentAlt,
-  faBars,
-  faBell,
-  faClock,
-  faAddressBook,
-  faTty,
-  faPhoneVolume,
-  faEnvelope,
-  faHeadset,
-  faWindowMinimize,
-  faWindowClose,
-  faCheckCircle,
-  faChevronRight,
-} from '@fortawesome/free-solid-svg-icons'
+import './config/FontAwesome'
 
 Vue.use(Vuetify)
-
 if (!process.env.IS_WEB) Vue.use(require('vue-electron'))
-Vue.http = Vue.prototype.$http = axios
-Vue.config.productionTip = false
-
-library.add(faFontAwesome)
-library.add(faBars)
-library.add(faAddressBook)
-library.add(faBell)
-library.add(faClock)
-library.add(faCommentAlt)
-library.add(faEnvelope)
-library.add(faHeadset)
-library.add(faPhoneVolume)
-library.add(faTty)
-library.add(faWindowMinimize)
-library.add(faWindowClose)
-library.add(faCheckCircle)
-library.add(faChevronRight)
-library.add(faWhatsapp)
-
-Vue.component('font-awesome-icon', FontAwesomeIcon)
-
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
 new Vue({
-  components: { App },
+  i18n,
   router,
   store,
-  template: '<App/>'
+  render: h => h(App)
 }).$mount('#app')
